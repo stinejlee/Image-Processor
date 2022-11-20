5ec72b9940498536076140c018e887c892e57687
Christine Lee <lee.christine@northeastern.edu>
2022-11-20 18:33:37 -0500

    ImagePanel class

---
+++ src/view/ImagePanel.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.*;
+
+import model.IImage;
+import model.Image;
+import model.ImageProcessingModelState;
+
+public class ImagePanel extends JPanel {
+
+  private ImageProcessingModelState modelState;
+  private IImage image;
+}
