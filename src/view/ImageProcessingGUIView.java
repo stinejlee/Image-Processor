7907db7b14d6113c7e40475be4c7c0abbf3a3f8f
Anzchen <Anthonyzchen@yahoo.com>
2022-11-20 21:00:04 -0500

    STARTING POINT

--- src/view/ImageProcessingGUIView.java
+++ src/view/ImageProcessingGUIView.java
@@ -1,5 +1,9 @@
 package view;
 
+import java.awt.event.ActionListener;
+
+import javax.swing.event.ListSelectionListener;
+
 public interface ImageProcessingGUIView extends ImageProcessingView {
   /**
    * Refresh the screen. This is called when the something on the
@@ -7,5 +11,23 @@ public interface ImageProcessingGUIView extends ImageProcessingView {
    */
   void refresh();
 
+  /**
+   * Transmit an error message to the view, in case
+   * the command could not be processed correctly
+   *
+   * @param error
+   */
+  void showErrorMessage(String error);
 
+  /**
+   * Provide the view with an action listener for
+   * the button that should cause the program to
+   * process a command. This is so that when the button
+   * is pressed, control goes to the action listener
+   *
+   * @param actionEvent
+   */
+  void setCommandButtonListener(ActionListener actionEvent);
+
+  void setListSelectionListener(ListSelectionListener actionEvent);
 }
---
+++ src/view/RedHistogramPanel.java
@@ -0,0 +1,6 @@
+package view;
+
+import javax.swing.*;
+
+public class RedHistogramPanel extends JPanel {
+}
